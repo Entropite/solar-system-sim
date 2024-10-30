46b3cfd39165edcf44bebd301a3170e00b985437
ToppleKek <topkek@topplekek.xyz>
2024-10-30 13:54:59 -0400

    XR support
    Move with the left analog stick of an Oculus touch controller. Camera
    still can be controlled with the keyboard/mouse. The camera script
    disables XR controller models in desktop mode.

--- Solar System/Assets/Resources/Scripts/CameraMovement.cs
+++ Solar System/Assets/Resources/Scripts/CameraMovement.cs
@@ -17,35 +17,48 @@ public class CameraMovement : MonoBehaviour
     private float verticalRotation = 0.0f;
 
     // Start is called before the first frame update
-    void Start() { }
+    void Start()
+    {
+        // Disable XR controllers if not running in XR. If they are enabled, you can stil see the controller rays in desktop mode.
+        if (!UnityEngine.XR.XRSettings.enabled)
+        {
+            Debug.Log("XR is disabled");
+            GameObject.Find("Left Controller").SetActive(false);
+            GameObject.Find("Right Controller").SetActive(false);
+        }
+    }
 
     // Update is called once per frame
     void Update()
     {
-        // transform.Translate(Vector3.forward * 10 * Time.deltaTime);
-        //rotating with mouse
-        horizontalRotation += horizontalMouseSensitivity * Input.GetAxis("Mouse X");
-        verticalRotation -= verticalMouseSensitivity * Input.GetAxis("Mouse Y");
-        transform.eulerAngles = new Vector3(verticalRotation, horizontalRotation, 0);
+        // Don't try to move the camera when in XR
+        if (!UnityEngine.XR.XRSettings.enabled)
+        {
+            // transform.Translate(Vector3.forward * 10 * Time.deltaTime);
+            //rotating with mouse
+            horizontalRotation += horizontalMouseSensitivity * Input.GetAxis("Mouse X");
+            verticalRotation -= verticalMouseSensitivity * Input.GetAxis("Mouse Y");
+            transform.eulerAngles = new Vector3(verticalRotation, horizontalRotation, 0);
 
-        //moving with wasd
+            //moving with wasd
 
-        //detect keypresses/releases
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.Translate(Vector3.forward * forwardSpeed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(Vector3.left * sidewaysSpeed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.Translate(Vector3.back * forwardSpeed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(Vector3.right * sidewaysSpeed * Time.deltaTime);
+            //detect keypresses/releases
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.Translate(Vector3.forward * forwardSpeed * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Translate(Vector3.left * sidewaysSpeed * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.Translate(Vector3.back * forwardSpeed * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Translate(Vector3.right * sidewaysSpeed * Time.deltaTime);
+            }
         }
     }
 }
